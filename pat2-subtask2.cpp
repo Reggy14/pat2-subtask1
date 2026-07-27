199b5daf4bcee702c0f8c5cbcdc08666d3b66f4b
Reggy Ntaopane <reggyntaopane14@gmail.com>
2026-07-27 17:36:49 +0200

    add console header

--- pat2-subtask2.cpp
+++ pat2-subtask2.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <string>
 #include <cctype>
@@ -6,6 +7,7 @@ using namespace std;
 
 int main()
 {
+    cout << "\nOutput Morse code:" << endl;
     char letters[] = {
         'A','B','C','D','E','F','G','H','I','J','K','L','M',
         'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
